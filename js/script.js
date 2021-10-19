a5f16d59f99cd2b17f197cdab84aebc9d37a5e96
DADEE SAMUEL <dadeesamuel@gmail.com>
2021-10-19 16:14:33 +0100

    putting more function

--- js/script.js
+++ js/script.js
@@ -17,7 +17,11 @@ function callback(){
 }*/
 
 
-let sentence = prompt("kindly input a sentence");
+
+
+
+
+/*let sentence = prompt("kindly input a sentence");
 let firstCharacter = sentence.charAt(0);
 let secondCharacter = sentence.charAt(sentence.length -1);
 let characters = firstCharacter.concat(secondCharacter);
@@ -29,5 +33,68 @@ let divide = length1/2 ;
 let rounding = Math.floor(divide);
 let res = sentence.charAt(rounding);
 let newSentence = res.concat(sentence);
-let finalReverse =  [...newSentence].reverse().join("");
+let finalReverse = [...newSentence].reverse().join("");
 console.log (rounding, divide, res);
+
+function Q1(upperCharacter){
+    return upperCharacter;
+}
+
+function Q2(rev){
+    return rev;
+}
+function Q3(){
+    return ;
+}
+function Q4(){
+    return ;
+}
+
+
+/*$(document).ready(function() {
+      
+      $("p").click(function() {
+        document.writeIn(sentence);
+      });
+      $("a").click(function() {
+        document.write(finalReverse);
+      });
+    })*/
+
+
+
+
+    
+   // prompting a user for a sentence;
+   let sentence = prompt("kindly input a sentence");
+   console.log (sentence);
+
+   //first and last character of the sentence
+ function character(sentence) {
+    let firstCharacter = sentence.charAt(0).toUpperCase();
+    let lastCharacter= sentence.charAt(sentence.length -1).toUpperCase();
+    let concat1 = firstCharacter.concat(lastCharacter);
+    return concat1;
+ document.write(character(sentence));
+ }
+  // second function to reverse the order of the first function
+ function reversedCharacter(character,sentence){
+    let rev = [...character(sentence)].reverse().join("");
+    return rev ;
+    document.write(rev);
+ }
+// third function to call other two functions
+function callOtherFunction(sentence,character,reversedCharacter) {
+    let newConcat = sentence.concat(rev);
+    return newConcat;
+}
+
+// fourth function to count the sentence
+function counting(sentence) {
+    let length1 = sentence.length;
+let divide = length1/2 ;
+let rounding = Math.floor(divide);
+let res = sentence.charAt(rounding);
+let newSentence = res.concat(sentence);
+let finalReverse = [...newSentence].reverse().join("");
+}
